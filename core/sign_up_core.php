7c70aae3e9a0bc50ab33a70165bb505a8b3cab46
prarona06 <prarona.dutta.06@gmail.com>
2025-08-27 15:18:29 +0600

    add some conditions

--- core/sign_up_core.php
+++ core/sign_up_core.php
@@ -2,31 +2,48 @@
 
 <?php
 $data_email = 'prity123@gmail.com';
-$data_password ='123pass';
+$data_password ='123456789123456789';
 
-$signup_email = $_POST['email'];
-$signup_password = $_POST['password'];
+$signup_email = $_REQUEST['email'];
+$signup_password = $_REQUEST['password'];
+
+
+
+if (empty($signup_email))  {
+  header('Location: ../index.php?email=empty');
+}
+
+
+
+if (!empty($signup_password)){
+  $pLen =strLen($signup_password);
+  if($pLen <15){
+    if ($signup_password !== $data_password) {
+header('Location ../index.php?password=wrong');
+    } 
+  echo'password is correct';
+ }else{
+    header('Location:../index.php?password=short');
+  }
+
+}else{
+  header ('Location: ../index.php?password=empty');
+}
+
+
+
+
 
 echo "Email: " . $signup_email . '<br>';
 echo "Password: " . $signup_password;
 
-if($signup_email == $data_email  &&  $signup_password == $data_password ) {
 
-echo 'success';
-
-}else{
-    echo 'failed';
+if ($signup_email = $data_email &&  $signup_password = $data_password){
+  echo' success';
+}else
+{ 
+  echo  'failed';
 }
-
-
-if (empty($signup_email) || empty($signup_password )){
      
-   $erros = [
-    
-     'email_required' => 'required',
-    'password_required' => 'required',
-    
-   ];
-}
-
+  
 ?>
--- index.php
+++ index.php
@@ -17,13 +17,29 @@
         <h3>Login</h3>
         <form action ="./core/sign_up_core.php" method = "post">
             <div class="mb-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email"  name ="email" class="form-control" id="email" placeholder="Enter your email">
+                <label for="email" class="form-label">Email address <span class="text-danger">*</span></label>
+                <input type="email"  name ="email" class="form-control <?php  if (isset ($_REQUEST ['email']) && $_REQUEST ['email'] == 'empty') { echo 'is-invalid';} ?>" id="email" placeholder="Enter your email">
          
+
+                <?php 
+                if (isset ($_REQUEST['email']) && $_REQUEST ['email'] = 'empty') {
+                      echo '<p class="text-danger">Email is Required!</p>';
+                }
+                ?>
+                              
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
                 <input type="password" name ="password" class="form-control" id="password" placeholder="Enter your password">
+           <?php  
+           if(isset($_REQUEST ['password']) && $_REQUEST['password'] = 'wrong'){
+            echo'<p class="text-danger">Password is wrong</p>';
+           }else if((isset($_REQUEST ['password']) && $_REQUEST['password'] = 'short'))
+   echo'<p class="text-danger">Password is too short</p>';
+           
+           
+           ?>
+           
             </div>
             <div class="d-grid mb-3">
                 <button type="submit" class="btn btn-login">Login</button>
@@ -33,10 +49,7 @@
             </div>
         </form>
         <hr>
-        <div class="text-center">
-            <p>Don't have an account?</p>
-            <a href="#" class="btn btn-success">Create Account</a>
-        </div>
+        
     </div>
 
     
